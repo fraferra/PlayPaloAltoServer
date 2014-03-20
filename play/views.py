1d3a71bad6594a6053795f603e599080e6c1c336
fraferra <fraferra@cisco.com>
2014-03-19 17:02:59 -0700

    social-auth

--- play/views.py
+++ play/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.shortcuts import render, render_to_response, redirect
+from django.http import HttpResponseRedirect, HttpResponse
+
+
+def home(request):
+    return render(request, 'play/home.html')
