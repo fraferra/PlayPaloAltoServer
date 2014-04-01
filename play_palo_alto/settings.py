6160aa23b919cccee9abc7d53cbd5545ccfdef96
fraferra <fraferra@cisco.com>
2014-04-01 15:00:08 -0700

    different databases

--- play_palo_alto/settings.py
+++ play_palo_alto/settings.py
@@ -7,7 +7,7 @@ TEMPLATE_DEBUG = DEBUG
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
-env = os.environ.get("APP", "local")
+var = os.environ.get("APP", "local")
 MANAGERS = ADMINS
 if var =='local':
     DATABASES = {
