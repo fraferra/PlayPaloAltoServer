b6a5c0cd3b9175a5e906189c4150ba8342839a88
fraferra <fraferra@cisco.com>
2014-04-11 15:01:49 -0700

    chanegs

--- play/views.py
+++ play/views.py
@@ -210,7 +210,7 @@ def api_my_events(request):
         for event in events:
             list_events.append({'name':event.title, 'points':event.points, 'experience':event.experience, 'location':event.location})
         data= {'user':user.username, 'score':player.score, 'experience':player.experience,
-               'picture_url':player.picture_url 'events':list_events}
+               'picture_url':player.picture_url, 'events':list_events}
         data = simplejson.dumps(data)
         return HttpResponse(data, mimetype='application/json')
 
@@ -306,92 +306,3 @@ def api_events(request):
 
 
 
-
-
-'''
-
-@csrf_exempt
-def api_login(request):
-    username, password =authenticationFra(request)
-    user = authenticate(username=username,  password=password)
-    auth_login(request,user)
-    if user.is_authenticated():
-        player=Player.objects.get(user=user)
-
-        data= {'user':user.username, 'player':player.score}
-        data = simplejson.dumps(data)
-        return HttpResponse(data, mimetype='application/json')
-    else: 
-        return HttpResponse('not auth')
-
-@csrf_exempt
-def leaderboard(request):
-    username, password =authenticationFra(request)
-    user = authenticate(username=username,  password=password)
-    auth_login(request,user)
-    if user.is_authenticated():
-        player=Player.objects.get(user=user)
-        players=Player.objects.all().order_by('score')
-        list_of_players=[]
-        for other_player in players:
-            list_of_players.append({'player':other_player.user.username, 'player_experience':other_player.experience})
-
-        data= {'user':user.username, 'score':player.score, 'players':list_of_players}
-        data = simplejson.dumps(data)
-        return HttpResponse(data, mimetype='application/json')
-    else: 
-        pass
-
-
-@csrf_exempt
-def coupons(request):
-    username, password =authenticationFra(request)
-    user = authenticate(username=username,  password=password)
-    auth_login(request,user)
-    if user.is_authenticated():
-        player=Player.objects.get(user=user)
-        #if request.method == 'GET':
-        id_coupon=request.GET.get('id_coupon','')
-        if len(id_coupon)!=0:
-            coupon=Coupon.objects.get(pk=id_coupon)
-            player.score=player.score-coupon.price
-            if player.score <0:
-                return HttpResponse('not enough points')
-            coupon.buyers.add(player)
-            coupon.save()
-            player.save()
-            data={'score':player.score}
-            data = simplejson.dumps(data)
-            return HttpResponse(data, mimetype='application/json')
-        coupons=Coupon.objects.all()
-        list_of_coupons=[]
-        for cou in coupons:
-            list_of_coupons.append({'name':cou.title, 'price':cou.price})
-        data= {'user':user.username, 'list_of_coupons':list_of_coupons}
-        data = simplejson.dumps(data)
-        return HttpResponse(data, mimetype='application/json')
-    else: 
-        return HttpResponse('not auth')
-
-
-@csrf_exempt
-def my_coupons(request):
-    username, password =authenticationFra(request)
-    user = authenticate(username=username,  password=password)
-    auth_login(request,user)
-    if user.is_authenticated():
-        player=Player.objects.get(user=user)
-        coupons=player.coupon_set.all()
-        list_of_coupons=[]
-        for coupon in coupons:
-            list_of_coupons.append({'name':coupon.title})
-        data= {'user':user.username, 'player':player.score, 'coupons':list_of_coupons}
-        data = simplejson.dumps(data)
-        return HttpResponse(data, mimetype='application/json')
-    else: 
-        return HttpResponse('not auth') 
-
-
-'''
-
-	
