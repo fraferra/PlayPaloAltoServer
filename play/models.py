898a5ab7418695d2fb62d16a52ef7a4de3c3bd4e
fraferra <fraferra@cisco.com>
2014-05-12 12:58:00 -0700

    coupons

--- play/models.py
+++ play/models.py
@@ -52,6 +52,7 @@ class Shop(models.Model):
     user=models.ForeignKey(User)
     title=models.CharField(max_length=100, null=True, default='Super shop!')
     location=models.CharField(max_length=100, null=True)
+    picture_url=models.CharField(max_length=200, null=True, default='/static/img/stanford.png')
     def __unicode__(self):  # Python 3: def __str__(self):
         return unicode(self.title) or u''   
 
@@ -68,6 +69,7 @@ class Coupon(models.Model):
     location=models.CharField(max_length=100, null=True)
     price=models.DecimalField(max_digits=4, decimal_places=0)
     buyers=models.ManyToManyField(Player, default=None, null=True)
+    picture_url=models.CharField(max_length=200, null=True, default='/static/img/stanford.png')
     shop=models.ForeignKey(Shop)
     coupons_released=models.DecimalField(max_digits=4, decimal_places=0, default=10)
     def __unicode__(self):  # Python 3: def __str__(self):
@@ -89,6 +91,11 @@ class Event(models.Model):
     def __unicode__(self):  # Python 3: def __str__(self):
         return unicode(self.title) or u'' 
 
+def assign_coupon_pic(sender, instance, created, **kwargs):
+    if created:
+        instance.picture_url=instance.shop.picture_url
+        instance.save()
+post_save.connect(assign_coupon_pic, sender=Coupon)
 
 def assign_event_pic(sender, instance, created, **kwargs):
     if created:
